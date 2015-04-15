ba62db050a034490d7cab5cade39fdae76a8488d
y-kurami <yosuke.kurami@gmail.com>
2015-04-16 05:09:44 +0900

    Add script for creating local Vim

--- runtest.sh
+++ runtest.sh
@@ -3,6 +3,40 @@
 VIMRC_FILE="vest/.vimrc"
 DRIVER_FILE="vest/_runner"
 RESULT_FILE="vest/test_result.log"
+VIM_BUILD=1
+VIM_INSTALL_DIR=`pwd`/local
+
+if [ "${VIM_BUILD}" -eq 1 ]; then
+  echo "`date "+[%Y-%m-%dT%H:%M:%S]"` Use local Vim."
+  if [ ! -d "./local" ]; then
+    echo "`date "+[%Y-%m-%dT%H:%M:%S]"` Installing Vim"
+    if [ ! -d "./vim" ]; then
+      echo "`date "+[%Y-%m-%dT%H:%M:%S]"` Clonning Vim source from Github"
+      git clone https://github.com/vim-jp/vim.git
+    fi
+    cd vim
+    ./configure --prefix=${VIM_INSTALL_DIR}
+    if [ ! $? -eq 0 ]; then
+      exit $?
+    fi
+    make
+    if [ ! $? -eq 0 ]; then
+      exit $?
+    fi
+    make install
+    if [ ! $? -eq 0 ]; then
+      exit $?
+    fi
+    echo "`date "+[%Y-%m-%dT%H:%M:%S]"` Vim was created successfully."
+    cd ..
+  fi
+  VIM_CMD="${VIM_INSTALL_DIR}/bin/vim"
+else
+  echo "`date "+[%Y-%m-%dT%H:%M:%S]"` Use system Vim."
+  VIM_CMD="vim"
+fi
+
+${VIM_CMD} --version
 
 if [ ! -d "./neobundle.vim" ]; then
   echo "`date "+[%Y-%m-%dT%H:%M:%S]"` Installing neobundle"
@@ -14,7 +48,7 @@ if [ -f "${RESULT_FILE}" ]; then
 fi
 
 echo "`date "+[%Y-%m-%dT%H:%M:%S]"` Run vesting."
-vim -u ${VIMRC_FILE} -s ${DRIVER_FILE}
+${VIM_CMD} -u ${VIMRC_FILE} -s ${DRIVER_FILE}
 echo "`date "+[%Y-%m-%dT%H:%M:%S]"` Done."
 echo "`date "+[%Y-%m-%dT%H:%M:%S]"` Result: (${RESULT_FILE})"
 cat ${RESULT_FILE}
